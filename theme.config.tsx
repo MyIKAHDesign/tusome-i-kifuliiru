f73e685f09ef3143c809314c5c343686ff44cf39
MyIKAHDesign <99662370+MyIKAHDesign@users.noreply.github.com>
2023-01-22 14:36:04 +0200

    Nayongera ibndu bingi mu theme.config

--- theme.config.tsx
+++ theme.config.tsx
@@ -13,6 +13,51 @@ const config: DocsThemeConfig = {
   footer: {
     text: 'Tusome i Kifuliiru',
   },
+   
+}
+
+export default {
+  useNextSeoProps() {
+    const { route } = useRouter()
+    if (route !== '/') {
+      return {
+        titleTemplate: '%s – Tusome i KIfuliiru'
+      }
+    }
+  },
+}
+
+export default {
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Tusome i Kifuliiru. Kifuliiru ndeto yitu" />
+      <meta property="og:description" content="Tusome indeto yitu Kifuliiru mu Kifuliiru. Kifuliiru ndeto yitu, ndeto ngale." />
+    </>
+  ),
+}
+
+export default {
+  banner: {
+    key: '2.0-release',
+    text: <a href="https://kifuliiru.vercel.app/" target="_blank">
+      🎉 Tusome i Kifuliiru 2.0. Wangakoli yaandika naho kifuliiru.vercel.comm.  Lenga hano →
+    </a>,
+  },
+}
+
+export default {
+  footer: {
+    text: <span>
+      MIT {new Date().getFullYear()} © <a href="https://kifuliiru.vercel.app/" target="_blank">Tusome i KIfuliiru</a>.
+    </span>,
+  }
+}
+
+export default {
+  search:{
+    placeholder: "Looza hano...",
+  }
 }
 
 export default config
