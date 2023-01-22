07234d8d4c8fda7bfccacc83ef720b9b141840c6
MyIKAHDesign <99662370+MyIKAHDesign@users.noreply.github.com>
2023-01-22 15:34:18 +0200

    Nahindula i icon ye Discord nabiika ye' Twitter

--- theme.config.tsx
+++ theme.config.tsx
@@ -9,8 +9,8 @@ const config: DocsThemeConfig = {
   },
   chat: {
     link: 'https://twitter.com/ibufuliiru',
-    icon: <svg width="24" height="24" viewBox="0 0 256 256"><path fill="currentColor" d="m231.9 169.8l-94.8 65.6a15.7 15.7 0 0 1-18.2 0l-94.8-65.6a16.1 16.1 0 0 1-6.4-17.3L45 50a12 12 0 0 1 22.9-1.1L88.5 104h79l20.6-55.1A12 12 0 0 1 211 50l27.3 102.5a16.1 16.1 0 0 1-6.4 17.3Z"></path></svg>,
-  
+    icon: <svg height="32" style="overflow:visible;enable-background:new 0 0 32 32" viewBox="0 0 32 32" width="32" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><g id="Twitter_1_"><g id="Twitter"><circle cx="16" cy="16" id="BG" r="16" style="fill:#5FCCFF;"/><path d="M22.1,12.1c0.7-0.4,1.2-1.1,1.4-1.8c-0.6,0.4-1.3,0.6-2.1,0.8        c-0.6-0.6-1.5-1-2.4-1c-1.8,0-3.3,1.5-3.3,3.3c0,0.3,0,0.5,0.1,0.7c-2.7-0.1-5.2-1.4-6.8-3.4c-0.3,0.5-0.4,1.1-0.4,1.7        c0,1.1,0.6,2.1,1.5,2.7c-0.5,0-1-0.2-1.5-0.4v0c0,1.6,1.1,2.9,2.6,3.2C11,17.9,10.7,18,10.4,18c-0.2,0-0.4,0-0.6-0.1        c0.4,1.3,1.6,2.3,3.1,2.3c-1.1,0.9-2.5,1.4-4.1,1.4c-0.3,0-0.5,0-0.8,0c1.4,0.9,3,1.4,4.7,1.5h0.8c5.7-0.3,8.9-5.1,8.9-9.3        c0-0.1,0-0.3,0-0.4c0.6-0.5,1.2-1,1.6-1.7v0C23.4,11.8,22.8,12,22.1,12.1z" id="Bird" style="fill:#F4F4F4;"/></g></g></g></svg>,
+
   },
   docsRepositoryBase: 'https://github.com/MyIKAHDesign/tusome-i-kifuliiru',
   // footer: {
@@ -20,7 +20,7 @@ const config: DocsThemeConfig = {
     const { route } = useRouter()
     if (route !== '/') {
       return {
-        titleTemplate: '%s – Tusome i KIfuliiru'
+        titleTemplate: '%s – Tusome i Kifuliiru'
       }
     }
   },
@@ -39,7 +39,7 @@ const config: DocsThemeConfig = {
   },
   footer: {
     text: <span>
-      MIT {new Date().getFullYear()} © <a href="https://kifuliiru.vercel.app/" target="_blank">Tumenye Ibufuliiru</a>. Guno mukolwa guliri hano higulu tumenye bingi ku Kifuliiru.
+      AYIVUGWE KABEMBA {new Date().getFullYear()} © <a href="https://kifuliiru.vercel.app/" target="_blank">Tumenye Ibufuliiru</a>. Guno mukolwa guliri hano higulu tumenye bingi ku Kifuliiru.
     </span>,
   },
   search:{
