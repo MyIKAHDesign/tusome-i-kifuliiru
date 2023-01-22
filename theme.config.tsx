479462fd02ea73541c013d52e29bb23b5a885a9d
MyIKAHDesign <99662370+MyIKAHDesign@users.noreply.github.com>
2023-01-22 15:14:15 +0200

    Nayongera i lien ku Twitter ne'bindi bingi mu theme.config

--- theme.config.tsx
+++ theme.config.tsx
@@ -8,7 +8,9 @@ const config: DocsThemeConfig = {
     link: 'https://github.com/MyIKAHDesign/tusome-i-kifuliiru',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://twitter.com/ibufuliiru',
+    icon: <svg width="24" height="24" viewBox="0 0 256 256"><path fill="currentColor" d="m231.9 169.8l-94.8 65.6a15.7 15.7 0 0 1-18.2 0l-94.8-65.6a16.1 16.1 0 0 1-6.4-17.3L45 50a12 12 0 0 1 22.9-1.1L88.5 104h79l20.6-55.1A12 12 0 0 1 211 50l27.3 102.5a16.1 16.1 0 0 1-6.4 17.3Z"></path></svg>,
+  
   },
   docsRepositoryBase: 'https://github.com/MyIKAHDesign/tusome-i-kifuliiru',
   // footer: {
@@ -37,11 +39,15 @@ const config: DocsThemeConfig = {
   },
   footer: {
     text: <span>
-      MIT {new Date().getFullYear()} © <a href="https://kifuliiru.vercel.app/" target="_blank">Tusome i KIfuliiru</a>.
+      MIT {new Date().getFullYear()} © <a href="https://kifuliiru.vercel.app/" target="_blank">Tumenye Ibufuliiru</a>. Guno mukolwa guliri hano higulu tumenye bingi ku Kifuliiru.
     </span>,
   },
   search:{
     placeholder: "Looza hano...",
+    emptyResult: "Ndabyo twaloonga",
+    loading: "Tugweeti tugalooza",
+    error:"Hali ibyagendeka buligo",
+
   },
 }
 
