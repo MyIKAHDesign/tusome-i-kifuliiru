648b75a4a2fe6d6d8a6f695d484038e20ef96534
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2024-07-05 18:38:02 -0700

    Nayongera agandi mugambo mu description

--- theme.config.tsx
+++ theme.config.tsx
@@ -24,7 +24,7 @@ const config: DocsThemeConfig = {
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta property="og:title" content="Tusome i Kifuliiru mu Kifuliiru" />
-      <meta property="og:description" content="Tusome indeto yitu Kifuliiru mu Kifuliiru. Kifuliiru ndeto yitu, ndeto ngale. Learn the Kifuliiru language in Kifuliiru.
+      <meta property="og:description" content="Kifuliiru. Kifuliiru ndeto ye'Bufuliiru, ndeto ya'ABafuliiru. Tusome indeto yitu Kifuliiru mu Kifuliiru. Kifuliiru ndeto yitu, ndeto ngale. Learn the Kifuliiru language in Kifuliiru.
       This website is here to learn the Kifuliiru language and to promote it. The Kifuliiru language is a language spoken in DRCongo, the Congo-Kinshasa; in the Eastern part, in Uvira territory, South Kivu province. 
       You may have been asking yourself about what this language is? How can I learn it? Here is a platform for the Kifuliiru language for learners or students and teachers as well." />
       <meta property="og:image" content=".github/DRCongo.png" />
