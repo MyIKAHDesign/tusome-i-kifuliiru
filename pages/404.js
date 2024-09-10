227836fb4219c0d0e1f073aef8a5374e267de6ce
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2024-09-09 17:11:36 -0700

    Add styling on 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,21 +1,18 @@
 import Link from 'next/link';
+import './styles.css'; // Import your CSS file for styling
 
 export default function Custom404() {
   return (
-    <>
-      <h1>
-        404.
-      </h1>
-      <p>
+    <div className="container">
+      <h1 className="error-title">404.</h1>
+      <p className="error-message">
         Ngisi biindu ushuba mulooza ndabyo twaloonga. Haliko bigaba
         bikahamikizibwa mu kindi kibaaja, looza kandi li uhinduule ku
         ndondeero. Kongwa.
       </p>
       <Link href="/index">
-        <button>
-          Hinduula ku ndondeero
-        </button>
+        <button className="home-button">Hinduula ku ndondeero</button>
       </Link>
-    </>
+    </div>
   );
 }
