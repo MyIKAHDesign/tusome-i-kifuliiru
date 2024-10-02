2260ddb9630fa3c517ab0c410002a5a2df85bc1a
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2024-10-01 23:55:12 -0700

    Add logo image

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,33 +3,35 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
+  //   logo: (<img src="kifuliiru.png"> <span>Tusome i Kifuliiru</span> ,
 
- 
-//   logo: (<img src="kifuliiru.png"> <span>Tusome i Kifuliiru</span> ,
+  //   <style jsx>{`
+  //     img {
+  //       mask-image: linear-gradient(
+  //         60deg,
+  //         black 25%,
+  //         rgba(0, 0, 0, 0.2) 50%,
+  //         black 75%
+  //       );
+  //       mask-size: 400%;
+  //       mask-position: 0%;
+  //     }
+  //     img:hover {
+  //       mask-position: 100%;
+  //       transition:
+  //         mask-position 1s ease,
+  //         -webkit-mask-position 1s ease;
+  //     }
+  //   `}</style>
 
-//   <style jsx>{`
-//     img {
-//       mask-image: linear-gradient(
-//         60deg,
-//         black 25%,
-//         rgba(0, 0, 0, 0.2) 50%,
-//         black 75%
-//       );
-//       mask-size: 400%;
-//       mask-position: 0%;
-//     }
-//     img:hover {
-//       mask-position: 100%;
-//       transition:
-//         mask-position 1s ease,
-//         -webkit-mask-position 1s ease;
-//     }
-//   `}</style>
+  // </img>
+  //   ),
 
-// </img>
-//   ),
-
-logo: <span>Tusome i Kifuliiru</span>,
+  logo: (
+    <img src="kifuliiru.png" alt="Kifuliiru logo">
+      <span>Tusome i Kifuliiru</span>{" "}
+    </img>
+  ),
 
   //Remove GitHub link and icon on website. To display it, uncomment these lines below.
 
@@ -80,14 +82,13 @@ logo: <span>Tusome i Kifuliiru</span>,
       <div>
         <span>
           Tusome i Kifuliiru {new Date().getFullYear()} ©{" "}
-          <a href="https://kifuliiru.net/"> Tumenye Ibufuliiru </a>  
+          <a href="https://kifuliiru.net/"> Tumenye Ibufuliiru </a>
         </span>
-       
+
         <span>
           <a href="https://ayivugwekabemba.me" target="_blank">
-           By Ayivugwe Kabemba Mukome
+            By Ayivugwe Kabemba Mukome
           </a>
-          
         </span>
       </div>
     ),
