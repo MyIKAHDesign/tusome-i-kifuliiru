b765efc7ed86dfb0903d07151380273781744759
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2025-12-09 14:48:35 -0800

    feat: Center PageNavigation component for improved layout
    - Adjusted the positioning of the PageNavigation component from the right side to the center of the screen. This change enhances the visual layout and improves user interaction by providing a more balanced navigation experience.
    
    @wemp_progress page-navigation-position-update-001 Center PageNavigation component for improved layout
    @wemp_feature navigation-enhancements Enhance PageNavigation component layout and design

--- components/PageNavigation.tsx
+++ components/PageNavigation.tsx
@@ -309,7 +309,7 @@ export default function PageNavigation({ currentSlug }: PageNavigationProps) {
   }
   
   return (
-    <div className="fixed bottom-6 right-6 z-40 flex items-center gap-3">
+    <div className="fixed bottom-6 left-1/2 -translate-x-1/2 z-40 flex items-center gap-3">
       {prevSlug && (
         <a
           href={getHref(prevSlug)}
